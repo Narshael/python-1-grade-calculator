b073154bab436e252688be1c4ab36c069b3d7795
Narshael <Narsh2007@gmail.com>
2024-09-25 15:58:52 -0400

    add labs

--- a1.py
+++ a1.py
@@ -1,9 +1,17 @@
-print("hello")
-x = input("What is you name")
-print("Hello" + x)
-course_total = 0
-Final = int(input("What is your final grade"))
-final_weight = 0.2
+"""
+TODO
+    - remove the hello name
+    - configure the course total: add the remaining numbers, inputs etc..
 
-course_total = course_total + (Final * final_weight)
-print(100 * 0.20)
+"""
+course_total = 0
+Labs_completed = int(input("Enter the number of labs completed"))
+if Labs_completed > 6:
+    Labs_completed = 6
+lab_weight = 0.2
+
+course_total += Labs_completed/6 *lab_weight
+print(course_total)
+
+
+
