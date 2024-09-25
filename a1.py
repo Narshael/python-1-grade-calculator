12ce5a7fedd4422edd300545c4c587815f56d532
Narshael <Narsh2007@gmail.com>
2024-09-25 12:08:18 -0400

    started

---
+++ a1.py
@@ -0,0 +1,9 @@
+print("hello")
+x = input("What is you name")
+print("Hello" + x)
+course_total = 0
+Final = int(input("What is your final grade"))
+final_weight = 0.2
+
+course_total = course_total + (Final * final_weight)
+print(100 * 0.20)
